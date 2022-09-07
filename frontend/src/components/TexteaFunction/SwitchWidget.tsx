c66beccad8dd302ac434ea7032a4d97e033ba50e
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-09-08 03:00:45 +0800

    style(switch): formatting the code and correcting a `TS2339` error

--- frontend/src/components/TexteaFunction/SwitchWidget.tsx
+++ frontend/src/components/TexteaFunction/SwitchWidget.tsx
@@ -7,24 +7,32 @@ import {
   Switch,
 } from "@mui/material";
 import React from "react";
+import { JSONSchema7 } from "json-schema";
 
-const SwitchWidget = (prop: WidgetProps | any) => {
-  const [checked, setChecked] = React.useState<boolean>(!!prop.schema.default);
+interface WithWidgetJSONSchema7 extends JSONSchema7 {
+  widget?: string; // fix TS2339: Property 'widget' does not exist on type 'JSONSchema7'
+}
 
-  const _onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+const SwitchWidget = (props: WidgetProps) => {
+  const [checked, setChecked] = React.useState<boolean>(!!props.schema.default);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
-    prop.onChange(event.target.checked);
+    props.onChange(event.target.checked);
   };
 
-  let control = <Checkbox checked={checked} onChange={_onValueChange} />;
+  let control = <Checkbox checked={checked} onChange={onChange} />;
 
-  if (prop.schema.hasOwnProperty("widget") && prop.schema.widget === "switch") {
-    control = <Switch checked={checked} onChange={_onValueChange} />;
+  if (
+    "widget" in props.schema &&
+    (props.schema as WithWidgetJSONSchema7).widget === "switch"
+  ) {
+    control = <Switch checked={checked} onChange={onChange} />;
   }
   return (
     <FormControl fullWidth>
       <FormGroup>
-        <FormControlLabel control={control} label={prop.label || ""} />
+        <FormControlLabel control={control} label={props.label || ""} />
       </FormGroup>
     </FormControl>
   );
