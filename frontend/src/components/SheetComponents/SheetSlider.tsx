c36c53b3ac5444af00e173083693081d49702e9c
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-09-12 00:44:31 +0800

    fix(sheet slider): rollback

--- frontend/src/components/SheetComponents/SheetSlider.tsx
+++ frontend/src/components/SheetComponents/SheetSlider.tsx
@@ -1,41 +1,86 @@
 import { SheetInterface } from "./SheetInterface";
 import React from "react";
-import {
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  Switch,
-} from "@mui/material";
+import { FormControl, Grid, Input, Slider } from "@mui/material";
+import SliderValueLabel from "../Common/SliderValueLabel";
 
-export default function SheetSwitch(props: SheetInterface) {
-  const [checked, setChecked] = React.useState<boolean>(props.params.value);
+export default function SheetSlider(props: SheetInterface) {
+  let args = [];
+  if (props.widget.indexOf("[") === -1 || props.widget.indexOf("]") === -1) {
+    console.warn("slider syntax: slider[min, max, range]");
+  } else {
+    args = JSON.parse(props.widget.trim().split("slider")[1]);
+  }
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
+  const min = args[0] || 0;
+  const max = args[1] || 100;
+  const defaultStep = props.type === "integer" ? 1 : 0.1;
+  const step = args[2] || defaultStep;
+
+  const [value, setValue] = React.useState<
+    number | string | Array<number | string>
+  >(min);
+
+  const customSetValue = (value: number | string | Array<number | string>) => {
+    setValue(value);
     if (props.customChange instanceof Function) {
-      props.customChange(
-        props.params.row.id,
-        props.params.field,
-        event.target.checked
-      );
+      props.customChange(props.params.row.id, props.params.field, value);
+    }
+  };
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) =>
+    customSetValue(newValue);
+
+  const handleSliderInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    customSetValue(
+      event.target.value === ""
+        ? ""
+        : props.type === "integer"
+        ? parseInt(event.target.value)
+        : parseFloat(event.target.value)
+    );
+  };
+
+  const handleSliderInputBlur = () => {
+    if (value < min) {
+      customSetValue(min);
+    } else if (value > max) {
+      customSetValue(max);
     }
   };
 
   return (
     <FormControl fullWidth>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={checked}
-              onChange={onChange}
-              sx={{ margin: "0 auto" }}
-            />
-          }
-          label={""}
-          sx={{ width: "100%", margin: "0 auto" }}
-        />
-      </FormGroup>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === "number" ? value : min}
+            min={min}
+            max={max}
+            step={step}
+            onChange={handleSliderChange}
+            valueLabelDisplay="on"
+            components={{
+              ValueLabel: SliderValueLabel,
+            }}
+          />
+        </Grid>
+        <Grid item sx={{ maxWidth: "35%" }}>
+          <Input
+            value={value}
+            size="small"
+            inputProps={{
+              type: "number",
+              min: min,
+              max: max,
+              step: step,
+            }}
+            onChange={handleSliderInputChange}
+            onBlur={handleSliderInputBlur}
+          />
+        </Grid>
+      </Grid>
     </FormControl>
   );
 }
