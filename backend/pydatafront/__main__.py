77f5b02c11a0db3fb9e70f69498e27613130d810
himself65 <himself65@outlook.com>
2022-09-20 11:45:58 -0500

    fix: default host 0.0.0.0

--- backend/pydatafront/__main__.py
+++ backend/pydatafront/__main__.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyDataFront')
     parser.add_argument('this_class', type=str, help='this class', action='store')
     parser.add_argument('main_class', type=str, help='main class to import', action='store', default='functions')
-    parser.add_argument('--host', help='host of backend', action='store', default='localhost')
+    parser.add_argument('--host', help='host of backend', action='store', default='0.0.0.0')
     parser.add_argument('--port', help='port of backend', action='store', default='8080')
     parsed_sys_args = parser.parse_args(sys.argv)
     host: str = os.getenv("HOST", parsed_sys_args.host)
