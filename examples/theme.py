8cf5cdb39236ee552c4d3c1c93a6580ffba29869
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-09-28 17:16:18 +0800

    fix(theme test): theme config URL

--- examples/theme.py
+++ examples/theme.py
@@ -1,6 +1,6 @@
 from pydatafront.decorator import import_theme, set_global_theme, textea_export
 
-import_theme("http://127.0.0.1:8000/sunset_v2.yaml", "sunset")
+import_theme("https://yazawazi.moe/pdf_themes/sunset_v2.yaml", "sunset")
 set_global_theme("sunset")
 
 
