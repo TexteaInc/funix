84920e037d0a877875e77e6e18f418cf8870278d
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-03-01 02:34:51 +0800

    feat: use the same port for frontend and backend (#60)

--- backend/funix/__init__.py
+++ backend/funix/__init__.py
@@ -15,16 +15,14 @@ import_theme = decorator.import_theme
 set_global_theme = decorator.set_global_theme
 
 class OpenFrontend(Thread):
-    def __init__(self, host: str, front_port: int, backend_port: int):
+    def __init__(self, host: str, port: int):
         super(OpenFrontend, self).__init__()
         self.host = host
-        self.front_port = front_port
-        self.backend_port = backend_port
+        self.port = port
 
     def isServerOnline(self) -> bool:
         try:
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((self.host, self.backend_port))
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((self.host, self.front_port))
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((self.host, self.port))
             return True
         except:
             return False
@@ -32,7 +30,7 @@ class OpenFrontend(Thread):
     def run(self) -> None:
         while not self.isServerOnline():
             pass
-        webbrowser.open(f"http://{self.host}:{self.front_port}")
+        webbrowser.open(f"http://{self.host}:{self.port}")
 
 def __prep(main_class: typing.Optional[str]):
     decorator.enable_wrapper()
@@ -43,8 +41,7 @@ def __prep(main_class: typing.Optional[str]):
 
 def run(
     host: typing.Optional[str] = "localhost",
-    port: typing.Optional[int] = 8080,
-    front_port: typing.Optional[int] = 3000,
+    port: typing.Optional[int] = 3000,
     main_class: typing.Optional[str] = "functions",
     no_frontend: typing.Optional[bool] = False,
     no_browser: typing.Optional[bool] = False
@@ -53,23 +50,21 @@ def run(
     if host is None:
         host = "localhost"
     if port is None:
-        port = 8080
-    if front_port is None:
-        front_port = 3000
+        port = 3000
     if main_class is None:
         main_class = "functions"
     if no_frontend is None:
         no_frontend = False
     if no_browser is None:
         no_browser = False
-    print(f"Backend server running on http://{host}:{port}")
     if not no_frontend:
-        print(f"Frontend server running on http://{host}:{front_port}")
-        frontend_start = Thread(target=start, kwargs={"host": host, "port": front_port, "backend_port": port})
-        frontend_start.daemon = True
-        frontend_start.start()
+        print(f"Backend and frontend server running on http://{host}:{port}")
+    else:
+        print(f"Backend server running on http://{host}:{port}")
+    if not no_frontend and not no_browser:
+        start(host, port)
         if not no_browser:
-            web_browser_start = OpenFrontend(host=host, front_port=front_port, backend_port=port)
+            web_browser_start = OpenFrontend(host=host, port=port)
             web_browser_start.daemon = True
             web_browser_start.start()
     app.run(host=host, port=port)
--- backend/funix/frontend/__init__.py
+++ backend/funix/frontend/__init__.py
@@ -1,28 +1,27 @@
 import os
 import json
-from flask import Flask, send_from_directory
+from funix.app import app
+from flask import send_from_directory
 
 folder = os.path.abspath(os.path.join(os.path.abspath(__file__), "../../build"))
-frontend_app = Flask("funix.frontend")
 
-@frontend_app.route("/")
+@app.route("/")
 def send_index():
     return send_from_directory(folder, "index.html")
 
-@frontend_app.route("/<path:path>")
+@app.route("/<path:path>")
 def send_static(path):
     return send_from_directory(folder, path)
 
-@frontend_app.route("/static/css/<path:path>")
+@app.route("/static/css/<path:path>")
 def send_css_static(path):
     return send_from_directory(os.path.abspath(os.path.join(folder, "static/css/")), path)
 
-@frontend_app.route("/static/js/<path:path>")
+@app.route("/static/js/<path:path>")
 def send_js_static(path):
     return send_from_directory(os.path.abspath(os.path.join(folder, "static/js/")), path)
 
-def start(host: str, port: int, backend_port: int):
-    @frontend_app.route("/config/backend")
+def start(host: str, port: int):
+    @app.route("/config/backend")
     def send_backend_config():
-        return json.dumps({"backend": f"http://{host}:{backend_port}"})
-    frontend_app.run(host, port)
+        return json.dumps({"backend": f"http://{host}:{port}"})
