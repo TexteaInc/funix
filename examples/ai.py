fc6591aad91d77bfd445218888b5e481aae3fe72
forrest <forrest.bao@gmail.com>
2023-03-01 19:09:22 -0600

    update AI examples

--- examples/ai.py
+++ examples/ai.py
@@ -1,55 +1,66 @@
+import os # python native 
+
+import openai  # pip install openai
+openai.api_key = os.environ.get("OPENAI_KEY")
+
 from funix import funix
 from funix.hint import Image
 from funix import run
-import openai  # pip install openai
-import os
-
-openai.api_key = os.environ.get("OPENAI_KEY")
-
 
 @funix(
-    description="Set OpenAI API key",
-    argument_labels={
-        "api_key": "API key"
-    }
+  title="Set OpenAI API key",
+  argument_labels={
+    "api_key": "Enter your API key", 
+    "sys_env_var": "Use system environment variable"
+  }, 
+  conditional_visible=[
+    {
+      "if": {"sys_env_var": False},
+      "then": ["api_key"],
+    } ],
+    show_source=True
 )
-def set(api_key: str) -> str:
-    if openai.api_key is None:
-        openai.api_key = api_key
-        return "OpenAI API key is set! You may start using AI features now."
+def set(api_key: str="", sys_env_var:bool=True) -> str:
+    if sys_env_var:
+        return "OpenAI API key is set in your environment variable. Nothing changes."
     else:
-        return "OpenAI API key is already set! Not changed."
+        if api_key == "":
+            return "You entered an empty string. Try again."
+        else:
+            openai.api_key = api_key
+            return "OpenAI API key has been set via the web form! If it was set via an environment variable, it's been overwritten."
 
 
 @funix(
+    title="Dall-E",
     description="Generate an image by prompt with DALL-E",
-    argument_labels={
-        "prompt": "Prompt"
-    }
+    widgets={'Prompt': 'textarea'},
+    examples={'Prompt':["a cat on a red Tesla", 
+                        "a cat flying Boeing 747", 
+                        "a cat riding Saturn V rocket"]},
+    show_source=True
 )
-def dalle(prompt: str = "a cat") -> Image:
-    response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
+def dalle(Prompt: str = "a cat flying boeing 747") -> Image:
+    response = openai.Image.create(prompt=Prompt, n=1, size="1024x1024")
     return response["data"][0]["url"]
 
 
 @funix(
-    description="Ask some question to GPT-3",
-    argument_labels={
-        "question": "Question"
-    },
+    description="Ask a question to GPT-3",
+    widgets={'Question': 'textarea',
+             'temp':       'slider[0,1,0.1]', 
+             'max_tokens': 'slider[20,100,20]', 
+             'top_p':      'slider[0,1,0.1]'},
     show_source=True
 )
-def chatgpt(question: str = "Who is Cauchy?") -> str:
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt="Answer the question in one sentence: " + question,
-        temperature=0.5,
-        max_tokens=100,
-        top_p=0.3,
-        frequency_penalty=0.6,
-        presence_penalty=0.0,
+def GPT3(Question: str = "Who is Fermat?", 
+         temp: float=0.9, max_tokens: float=100, top_p: float=0.7) -> str:
+    response = openai.Completion.create(engine="davinci",
+        prompt= Question,
+        temperature=temp, max_tokens=max_tokens, top_p=top_p,
+        frequency_penalty=0.6, presence_penalty=0.0,
     )
-    return '{}'.format(response.choices[0].text)
+    return f'The answer is: {response.choices[0].text}'
 
 
 if __name__ == "__main__":
