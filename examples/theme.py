07b2d506ec5a8989e3ca85a08df788cbbd9d1505
forrest <forrest.bao@gmail.com>
2023-03-02 00:06:18 -0600

    update theme example with more widgets; bugs pending

--- examples/theme.py
+++ examples/theme.py
@@ -1,14 +1,24 @@
 from funix.decorator import import_theme, set_global_theme, funix
+import typing
 
 import_theme("./sunset_v2.yaml", "sunset")
 set_global_theme("sunset")
 
 
 @funix(
-    path="theme_test",
-    description="Sunset, the theme file is in the examples folder",
+    description="Widgets in the sunset theme. Theme file, sunset_v2.yaml is in the same directory as this script.",
+    widgets={'int_input_inputbox': 'inputbox', 
+             'bool_input_switch': 'switch', 
+             'literal_input_radio': 'radio'}
 )
-def theme_test(int_input: int, float_input: float, bool_input: bool, str_input: str):
+def theme_test(
+    int_input: int, 
+    float_input: float, 
+    int_input_inputbox: int, 
+    bool_input: bool, 
+    bool_input_switch: bool,
+    literal_input_radio: typing.Literal["a", "b", "c"],
+    str_input: str) :
     return {
         "int": int_input,
         "float": float_input,
