75ee9e59b6bec5d23cf576e08ddbd4e125871360
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-05-04 23:29:15 +0800

    fix: custom sheet widget roll back state

--- frontend/src/components/SheetComponents/SheetCheckBox.tsx
+++ frontend/src/components/SheetComponents/SheetCheckBox.tsx
@@ -1,14 +1,21 @@
 import { SheetInterface } from "./SheetInterface";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Checkbox,
   FormControl,
   FormControlLabel,
   FormGroup,
+  Switch,
 } from "@mui/material";
 
-export default function SheetCheckBox(props: SheetInterface) {
+export default function SheetCheckBox(
+  props: SheetInterface & { isSwitch: boolean }
+) {
   const [checked, setChecked] = React.useState<boolean>(props.params.value);
+
+  useEffect(() => {
+    setChecked(props.params.value);
+  }, [props.params.value]);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
@@ -19,17 +26,17 @@ export default function SheetCheckBox(props: SheetInterface) {
     );
   };
 
+  const controlElement = props.isSwitch ? (
+    <Switch checked={checked} onChange={onChange} sx={{ margin: "0 auto" }} />
+  ) : (
+    <Checkbox checked={checked} onChange={onChange} sx={{ margin: "0 auto" }} />
+  );
+
   return (
     <FormControl fullWidth>
       <FormGroup>
         <FormControlLabel
-          control={
-            <Checkbox
-              checked={checked}
-              onChange={onChange}
-              sx={{ margin: "0 auto" }}
-            />
-          }
+          control={controlElement}
           label={""}
           sx={{ width: "100%", margin: "0 auto" }}
         />
