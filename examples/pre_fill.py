591b48106a606a582fe899af791306693d95a283
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-08-13 08:43:28 +0800

    rewind: make `pre_fill` example back

---
+++ examples/pre_fill.py
@@ -0,0 +1,31 @@
+from funix import funix
+
+
+@funix()
+def first_action(x: int) -> int:
+    return x - 1
+
+
+@funix()
+def second_action(message: str) -> list[str]:
+    return message.split(" ")
+
+
+@funix()
+def third_action(x: int, y: int) -> dict:
+    return {
+        "x": x,
+        "y": y
+    }
+
+
+@funix(
+    pre_fill={
+        "a": first_action,
+        "b": (second_action, -1),
+        "c": (third_action, "x")
+    }
+)
+def final_action(a: int, b: str, c: int) -> str:
+    return f"{a} {b} {c}"
+
