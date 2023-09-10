53d25061d22cf1c2f3112d63e7136b91ddac55a8
forrest <forrest.bao@gmail.com>
2023-09-10 15:35:50 -0500

    update chatGPT multiturn example

--- examples/AI/chatGPT_muilti_turn.py
+++ examples/AI/chatGPT_muilti_turn.py
@@ -2,6 +2,8 @@ import os
 import openai 
 openai.api_key = os.environ.get("OPENAI_KEY")
 
+import IPython.display.HTML
+
 messages  = []  # list of dicts, dict keys: role, content, system 
 
 def print_messages_html(messages):
@@ -14,34 +16,19 @@ def print_messages_html(messages):
         printout += f'<div style="position: relative; left: {left}; width: 70%"><b>{name}</b>: {message["content"]}</div>'
     return printout
 
-def print_messages_markdown(messages):
-    printout = ""
-    for message in messages:
-        if message["role"] == "user":
-            mark, name =  "**", "You"
-        elif message["role"] == "assistant":
-            mark, name =  "_", "ChatGPT"
-        line = message["content"].strip().replace("\n", " ")
-        printout += f'{mark}{name}: {line}{mark}\n'
-        if message["role"] == "assistant":
-            printout += "\n"
-    return printout
-
 import funix 
-@funix.funix(
-    direction="column-reverse",
-    # show_source=True 
-)
-def ChatGPT_multi_turn(current_message: str)  -> funix.hint.HTML:
+@funix.funix(direction="column-reverse") # input is below log
+def ChatGPT_multi_turn(current_message: str)  -> IPython.display.HTML:
     current_message = current_message.strip()
     messages.append({"role": "user", "content": current_message})
     completion = openai.ChatCompletion.create(
         messages=messages,
-        model='gpt-3.5-turbo', 
-        max_tokens=100,
+        model='gpt-3.5-turbo' 
     )
     chatgpt_response = completion["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": chatgpt_response})
 
-    # return print_messages_markdown(messages)
     return print_messages_html(messages)
+
+
+
