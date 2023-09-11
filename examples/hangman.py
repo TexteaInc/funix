c7a48f8b5658670983a19e23b63ca4fb17717e91
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-09-11 12:11:25 +0800

    fix: use IPython's Markdown

--- examples/hangman.py
+++ examples/hangman.py
@@ -1,16 +1,19 @@
 # The hangman game with funix
 
 import funix
+from IPython.display import Markdown
 
 secret_word = "funix"
 used_letters = []
 
 
 @funix.funix(argument_labels={"letter": "Enter letter"})
-def guess_letter(letter: str) -> funix.hint.Markdown:
+def guess_letter(letter: str) -> Markdown:
     global used_letters  # state/session as global
     used_letters.append(letter)
     answer = "".join(
         [(letter if letter in used_letters else "_") for letter in secret_word]
     )
-    return f"### Hangman \n `{answer}` \n\n ---- \n ### Used letters \n {', '.join(used_letters)}"
+    return Markdown(
+        data=f"### Hangman \n `{answer}` \n\n ---- \n ### Used letters \n {', '.join(used_letters)}"
+    )
