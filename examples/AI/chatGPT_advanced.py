47fe45485d74d90e54a7f257de47f1a326482f7b
forrest <forrest.bao@gmail.com>
2023-09-12 08:18:17 -0500

    update examples

--- examples/AI/chatGPT_advanced.py
+++ examples/AI/chatGPT_advanced.py
@@ -1,30 +1,36 @@
+import os 
+import typing
+
 import openai 
 openai.api_key = os.environ.get("OPENAI_KEY")
 
 import funix 
 
-@funix.funix(
-        description="""Try the **ChatGPT** app in [Funix](http://funix.io)""",
-        argument_labels = {
-            "prompt" : "_What do you wanna ask?_",
-            "max_tokens": "**Length** of the answer",
-            "show_advanced": "Show advanced options",
-            "openai_key": "Use your own OpenAI key",
-            "model": "Choose a model",
-        },
-        widgets={"openai_key": "password", "model": "radio",
-                 "show_advanced": "checkbox", "show_verbose": "switch"},
-        conditional_visible=[
-            {"when": {"show_advanced": True,}, 
-             "show": ["max_tokens", "model", "openai_key"]}
-        ],
-        show_source=True 
-)
-def ChatGPT_advanced(prompt: str, 
-            show_advanced: bool = False,
-            model : typing.Literal['gpt-3.5-turbo', 'gpt-3.5-turbo-0301']= 'gpt-3.5-turbo',
-            max_tokens: range(100, 500, 50)=150,
-            openai_key: str = "use environment variable",)  -> str:      
+cfg = { # declarative configuration, all in one place
+    "description": """Try the **ChatGPT** app in [Funix](http://funix.io)""",
+    "argument_labels": {
+        "prompt" : "_What do you wanna ask?_",
+        "max_tokens": "**Length** of the answer",
+        "show_advanced": "Show advanced options",
+        "openai_key": "Use your own OpenAI key",
+        "model": "Choose a model",
+    },
+    "widgets": {"prompt": "textarea", 
+                "openai_key": "password", 
+                "model": "radio"},
+    "conditional_visible": [
+        {"when": {"show_advanced": True},
+         "show": ["max_tokens", "model", "openai_key"]}
+    ]
+}
+
+@funix.funix(**cfg)
+def ChatGPT_advanced(
+        prompt: str, 
+        show_advanced: bool = False,
+        model : typing.Literal['gpt-3.5-turbo', 'gpt-3.5-turbo-0301']= 'gpt-3.5-turbo',
+        max_tokens: range(100, 500, 50)=150,
+        openai_key: str = "use environment variable",)  -> str:      
     if openai_key != "use environment variable":
         openai.api_key = openai_key
     completion = openai.ChatCompletion.create(
--- examples/slider_table_plot.py
+++ examples/slider_table_plot.py
@@ -2,9 +2,10 @@ from typing import List
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
+import pandas 
+import pandera, pandera.typing 
+
 import funix
-
-
 @funix.funix(widgets={"a": "sheet", "b": ["sheet", "slider[0,5,0.2]"]})
 def table_plot(
     a: List[int] = [5, 17, 29], b: List[float] = [3.1415, 2.6342, 1.98964]
@@ -12,3 +13,19 @@ def table_plot(
     fig = plt.figure()
     plt.plot(a, b)
     return fig
+
+
+
+# Future implementation 
+
+# class MySchema(pandera.DataFrameModel):
+#     a: Series[int]
+#     b: Series[float]
+
+# def table_plot(df: pandera.typing.DataFrame[MySchema] = \
+#                    pandas.DataFrame[{"a": [5, 17, 29], 
+#                                      "b": [3.1415, 2.6342, 1.98964]}])\
+#             -> Figure:
+#     fig = plt.figure()
+#     plt.plot(a, b)
+#     return fig
