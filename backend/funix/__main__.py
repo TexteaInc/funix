bad790c80aa2539d41bfd0ea86908c3110415732
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-10-05 14:47:47 +0800

    chore: release `0.5.0`

--- backend/funix/__main__.py
+++ backend/funix/__main__.py
@@ -110,7 +110,7 @@ def cli_main():
 
     This function is called when you run `python -m funix` or `funix` from the command line.
     """
-    plac.call(main, version="Funix 0.4.8")
+    plac.call(main, version="Funix 0.5.0")
 
 
 if __name__ == "__main__":
