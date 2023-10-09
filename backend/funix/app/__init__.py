87f3be2c1e3e7ce430de0a88f0f516e458e0eec2
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-10-09 13:59:51 +0800

    feat: remove CORS

--- backend/funix/app/__init__.py
+++ backend/funix/app/__init__.py
@@ -5,8 +5,17 @@ Save the app instance here
 from secrets import token_hex
 
 import flask
-from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.secret_key = token_hex(16)
-CORS(app)
+app.config.update(
+    SESSION_COOKIE_PATH="/",
+    SESSION_COOKIE_SAMESITE="Lax",
+)
+
+
+@app.after_request
+def funix_auto_cors(response: flask.Response) -> flask.Response:
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE"
+    return response
