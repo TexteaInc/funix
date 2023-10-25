9c86d46f9cd7c2f2f7fc63d81d75f6bda4dc12f1
forrest <forrest.bao@gmail.com>
2023-10-25 16:40:39 -0500

    update examples for ipywidgets and pandera types

--- examples/slider_table_plot.py
+++ examples/slider_table_plot.py
@@ -1,9 +1,6 @@
-from typing import List
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-
-import pandas 
-import pandera, pandera.typing 
+from typing import List # Python native 
+import matplotlib.pyplot as plt  # the de facto standard for plotting in Python
+from matplotlib.figure import Figure # the matplotlib figure class
 
 import funix
 @funix.funix(widgets={"a": "sheet", "b": ["sheet", "slider[0,5,0.2]"]})
@@ -15,17 +12,20 @@ def table_plot(
     return fig
 
 
+# Future Implementation
 
-# Future implementation 
+# import pandas   # the de facto standard for tabular data in Python
+# import pandera  # the typing library for pandas dataframes
 
 # class MySchema(pandera.DataFrameModel):
-#     a: Series[int]
-#     b: Series[float]
+#     a: pandera.typing.Series[int]
+#     b: pandera.typing.Series[float]
 
+# funix.funix()
 # def table_plot(df: pandera.typing.DataFrame[MySchema] = \
-#                    pandas.DataFrame[{"a": [5, 17, 29], 
-#                                      "b": [3.1415, 2.6342, 1.98964]}])\
-#             -> Figure:
+#                    pandas.DataFrame({"a": [5, 17, 29], 
+#                                      "b": [3.1415, 2.6342, 1.98964]})
+#                 ) -> Figure:
 #     fig = plt.figure()
 #     plt.plot(a, b)
 #     return fig
