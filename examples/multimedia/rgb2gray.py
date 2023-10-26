042c198180d11a9d54209bc13ab1729b8c4addfd
forrest <forrest.bao@gmail.com>
2023-10-26 12:49:32 -0500

    update examples

--- examples/multimedia/rgb2gray.py
+++ examples/multimedia/rgb2gray.py
@@ -1,12 +1,13 @@
 import  io # Python's native 
 
 import PIL # the Python Image Library
+import IPython 
 import funix 
 
 @funix.funix(
     title="Convert color images to grayscale images",
 )
-def gray_it(image: funix.hint.BytesImage) -> funix.hint.Image:
+def gray_it(image: funix.hint.BytesImage) -> IPython.display.Image:
     img = PIL.Image.open(io.BytesIO(image))
     gray = PIL.ImageOps.grayscale(img) 
     output = io.BytesIO()
--- examples/slider_table_plot.py
+++ examples/slider_table_plot.py
@@ -1,31 +1,32 @@
 from typing import List # Python native 
-import matplotlib.pyplot as plt  # the de facto standard for plotting in Python
-from matplotlib.figure import Figure # the matplotlib figure class
+
+import matplotlib.figure, matplotlib.pyplot# the de facto standard for plotting in Python
 
 import funix
-@funix.funix(widgets={"a": "sheet", "b": ["sheet", "slider[0,5,0.2]"]})
-def table_plot(
-    a: List[int] = [5, 17, 29], b: List[float] = [3.1415, 2.6342, 1.98964]
-) -> Figure:
-    fig = plt.figure()
-    plt.plot(a, b)
-    return fig
 
-
-# Future Implementation
-
-# import pandas   # the de facto standard for tabular data in Python
-# import pandera  # the typing library for pandas dataframes
-
-# class MySchema(pandera.DataFrameModel):
-#     a: pandera.typing.Series[int]
-#     b: pandera.typing.Series[float]
-
-# funix.funix()
-# def table_plot(df: pandera.typing.DataFrame[MySchema] = \
-#                    pandas.DataFrame({"a": [5, 17, 29], 
-#                                      "b": [3.1415, 2.6342, 1.98964]})
-#                 ) -> Figure:
-#     fig = plt.figure()
-#     plt.plot(a, b)
+# @funix.funix(widgets={"a": "sheet", "b": ["sheet", "slider[0,5,0.2]"]})
+# def table_plot(
+#     a: List[int] = [5, 17, 29], b: List[float] = [3.1415, 2.6342, 1.98964]
+# ) -> Figure:
+#     fig = matplotlib.pyplot.figure()
+#     matplotlib.pyplot.plot(a, b)
 #     return fig
+
+
+# New Implementation after FEP 11
+
+import pandas   # the de facto standard for tabular data in Python
+import pandera  # the typing library for pandas dataframes
+
+class MySchema(pandera.DataFrameModel):
+    a: pandera.typing.Series[int]
+    b: pandera.typing.Series[float]
+
+@funix.funix()
+def table_plot(df: pandera.typing.DataFrame[MySchema] = \
+                   pandas.DataFrame({"a": [5, 17, 29], # default values
+                                     "b": [3.1415, 2.6342, 1.98964]})
+                ) -> matplotlib.figure.Figure:
+    fig = matplotlib.pyplot.figure()
+    matplotlib.pyplot.plot(df["a"], df["b"])
+    return fig
