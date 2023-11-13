15f127b1b56e7f8ea72bf3c2d6e05a8601841e9f
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-11-14 07:39:14 +0800

    feat: update chatgpt example

--- examples/AI/chatGPT_advanced.py
+++ examples/AI/chatGPT_advanced.py
@@ -21,31 +21,42 @@ import funix
 cfg = {  # declarative configuration, all in one place
     "description": """The ChatGPT app build in [Funix.io]""",
     "conditional_visible": [
-        {"when": {"show_advanced_options": True}, "show": ["max_tokens", "stream"]}
+        {"when": {"show_advanced_options": True}, "show": ["max_tokens"]}
     ],
     # "rate_limit": funix.decorator.Limiter.session(max_calls=2, period=60 * 60 * 24),
 }
 
 
 @funix.funix(**cfg)
-def ChatGPT_advanced(
+def ChatGPT_advanced_stream(
     prompt: str,
     show_advanced_options: bool = True,
-    stream: typing.Literal[True, False] = True,
-    max_tokens: range(100, 500, 50) = 150
+    max_tokens: range(100, 500, 50) = 150,
 ) -> IPython.display.Markdown:
-
-    client = openai.OpenAI() # defaults to os.environ.get("OPENAI_API_KEY")
+    client = openai.OpenAI()  # defaults to os.environ.get("OPENAI_API_KEY")
     response = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
         model="gpt-3.5-turbo",
-        stream=stream,
-        max_tokens=max_tokens
+        stream=True,
+        max_tokens=max_tokens,
     )
-    if stream: 
-        message = []
-        for chunk in response:
-            message.append(chunk.choices[0].delta.content or "")
-            yield "".join(message)
-    else: # no stream 
-        return response.choices[0].message.content
+    message = []
+    for chunk in response:
+        message.append(chunk.choices[0].delta.content or "")
+        yield "".join(message)
+
+
+@funix.funix(**cfg)
+def ChatGPT_advanced(
+    prompt: str,
+    show_advanced_options: bool = True,
+    max_tokens: range(100, 500, 50) = 150,
+) -> IPython.display.Markdown:
+    client = openai.OpenAI()  # defaults to os.environ.get("OPENAI_API_KEY")
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": prompt}],
+        model="gpt-3.5-turbo",
+        stream=False,
+        max_tokens=max_tokens,
+    )
+    return response.choices[0].text
