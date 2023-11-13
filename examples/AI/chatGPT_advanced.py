06a26c2bb510570b4bc9b7df188d5e02c1cbbd69
forrest <forrest.bao@gmail.com>
2023-11-13 17:32:46 -0600

    [demo] partially updating examples for streaming mode and openAI 1.1.1 API

--- examples/AI/chatGPT_advanced.py
+++ examples/AI/chatGPT_advanced.py
@@ -1,54 +1,51 @@
-import os
 import typing
-
-import ipywidgets
+import IPython
 
 import openai
-
-# openai.api_key = os.environ.get("OPENAI_KEY")
-# Disable this app from being called in public hosted examples
 
 import funix
 
 
-@funix.new_funix_type(
-    {
-        "name": "textarea",
-        "config": {
-            "rows": 6,
-        },
-    }
-)
-class PromptBox(str):
-    pass
+# @funix.new_funix_type(
+#     {
+#         "name": "textarea",
+#         "config": {
+#             "rows": 6,
+#         },
+#     }
+# )
+# class PromptBox(str):
+#     pass
 
 
 cfg = {  # declarative configuration, all in one place
-    "description": """Try the **ChatGPT** app in [Funix](http://funix.io), the minimalist way to build apps in Python. """,
-    "argument_labels": {
-        "prompt": "_What do you wanna ask?_",
-        "max_tokens": "**Length** of the answer",
-    },
+    "description": """The ChatGPT app build in [Funix.io]""",
     "conditional_visible": [
-        {"when": {"show_advanced_options": True}, "show": ["max_tokens", "model"]}
+        {"when": {"show_advanced_options": True}, "show": ["max_tokens", "stream"]}
     ],
-    "rate_limit": funix.decorator.Limiter.session(max_calls=2, period=60 * 60 * 24),
+    # "rate_limit": funix.decorator.Limiter.session(max_calls=2, period=60 * 60 * 24),
 }
 
 
 @funix.funix(**cfg)
 def ChatGPT_advanced(
-    prompt: PromptBox,
+    prompt: str,
     show_advanced_options: bool = True,
-    model: typing.Literal["gpt-3.5-turbo", "gpt-3.5-turbo-0613"] = "gpt-3.5-turbo",
-    max_tokens: range(100, 500, 50) = 150,
-    # openai_key: ipywidgets.Password = "use environment variable",
-) -> str:
-    # if openai_key != "use environment variable":
-    #     openai.api_key = openai_key
-    completion = openai.ChatCompletion.create(
+    stream: typing.Literal[True, False] = True,
+    max_tokens: range(100, 500, 50) = 150
+) -> IPython.display.Markdown:
+
+    client = openai.OpenAI() # defaults to os.environ.get("OPENAI_API_KEY")
+    response = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
-        model=model,
-        max_tokens=max_tokens,
+        model="gpt-3.5-turbo",
+        stream=stream,
+        max_tokens=max_tokens
     )
-    return completion["choices"][0]["message"]["content"]
+    if stream: 
+        message = []
+        for chunk in response:
+            message.append(chunk.choices[0].delta.content or "")
+            yield "".join(message)
+    else: # no stream 
+        return response.choices[0].message.content
