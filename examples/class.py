b711366ebda10149fa0d138e18de67ff87c64f02
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-11-22 13:17:01 +0800

    feat: support `funix_class_params`

--- examples/class.py
+++ examples/class.py
@@ -1,10 +1,11 @@
-from funix import funix_class
+from funix import funix_class, funix_class_params
 
 
 class A:
     def __init__(self):
         self.a = 1
 
+    @funix_class_params(title="okay")
     def abc(self, b: int, c: int) -> None:
         self.a = b + c
 
