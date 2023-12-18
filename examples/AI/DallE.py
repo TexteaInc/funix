3bffbe6e55e5c2715798a1633f161fdc5cd5f416
Forrest Bao <forrest.bao@gmai.com>
2023-12-17 22:57:37 -0600

    update openAI demos in compliance with new openAI API

--- examples/AI/DallE.py
+++ examples/AI/DallE.py
@@ -2,26 +2,28 @@ import os
 
 import openai  # pip install openai
 
-openai.api_key = os.environ.get("OPENAI_KEY")
+import IPython 
 
 import funix 
 
 
-@funix.funix(  # Funix.io, the laziest way to build web apps in Python
-    title="OpenAI: Dall-E",
-    description="""Generate an image with DALL-E in [Funix](http://funix.io), the minimalist way to build apps in Python. An OpenAI key needs to be set. A rate limit is applied. """,
-    rate_limit=funix.decorator.Limiter.session(max_calls=1, period=60 * 60 * 24),
-    show_source=True,
-)
-def dalle(
-    Prompt: str = "a cat on a red jeep",
-    openai_key: str = "using environment variable",
-) -> funix.hint.Image:
-    if openai_key != "using environment variable":
-        openai.api_key = openai_key
+cfg = {
+    'title': 'OpenAI: Dall-E',
+    'description': 'Generate an image with DALL-E in [Funix](http://funix.io), the minimalist way to build apps in Python. An OpenAI key needs to be set. A rate limit is applied. ',
+    'rate_limit': funix.decorator.Limiter.session(max_calls=1, period=60*60*24), 'show_source': True}
 
-    response = openai.Image.create(prompt=Prompt)
-    return response["data"][0]["url"]
+@funix.funix(**cfg)
+def dalle(
+    Prompt: str = "a cat on a red jeep"
+    ) -> IPython.display.Image:
+
+    client = openai.OpenAI() # defaults to os.environ.get("OPENAI_API_KEY")
+
+    response = client.images.generate(
+        prompt=Prompt,
+    )
+
+    return response.data[0].url
 
 
 # **Note:**
