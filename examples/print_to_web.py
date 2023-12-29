c65de88eef1c33bff2bc903b417ea468fe9b305f
Forrest Bao <forrest.bao@gmai.com>
2023-12-29 17:14:24 -0600

    update README with new examples

--- examples/print_to_web.py
+++
@@ -1,9 +0,0 @@
-from funix import funix
-@funix(
-    print_to_web=True
-)
-def my_main(x: str="123", debug:bool=True) -> None:
-    if debug:
-        print("### This is a debug message.")
-
-    return x 
