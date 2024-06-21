e56e175b835e919bfba8406da723a180b8d4d290
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2024-06-21 14:25:57 +0800

    fix: use inline code for stderr

--- backend/funix/app/websocket.py
+++ backend/funix/app/websocket.py
@@ -1,7 +1,7 @@
 """
 Websocket class to redirect
 """
-
+import sys
 from io import StringIO
 from json import dumps
 
@@ -10,10 +10,10 @@ class StdoutToWebsocket:
     """
     Stdout to websocket.
     """
-    
+
     encoding = "utf-8"
 
-    def __init__(self, ws):
+    def __init__(self, ws, is_err=False):
         """
         Initialize the StdoutToWebsocket.
 
@@ -21,26 +21,36 @@ class StdoutToWebsocket:
             ws (WebSocket): The websocket.
         """
         self.ws = ws
+        self.is_err = is_err
         self.value = StringIO()
+
+    def _get_html(self):
+        """
+        Get the html data.
+        """
+        value = self.value.getvalue()
+        if self.is_err and value.strip():
+            return f"`{value}`"
+        return value
 
     def write(self, data):
         """
         Write the data to the websocket.
         """
         self.value.write(data)
-        self.ws.send(dumps([self.value.getvalue()]))
+        self.ws.send(dumps([self._get_html()]))
 
     def writelines(self, data):
         """
         Write the lines to the websocket.
         """
         self.value.writelines(data)
-        self.ws.send(dumps([self.value.getvalue()]))
+        self.ws.send(dumps([self._get_html()]))
 
     def flush(self):
         """
         Flush the data to the websocket.
         """
         self.value.flush()
-        self.ws.send(dumps([self.value.getvalue()]))
+        self.ws.send(dumps([self._get_html()]))
         self.value = StringIO()
