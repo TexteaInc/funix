8633d6674e0b6305f2c7587d4bc25af7ae0aee0c
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2024-07-29 07:41:39 +0800

    fix: add `pagination` in `DataGrid`

--- frontend/src/components/FunixFunction/OutputComponents/OutputDataframe.tsx
+++ frontend/src/components/FunixFunction/OutputComponents/OutputDataframe.tsx
@@ -42,6 +42,8 @@ export default function OutputDataframe(props: {
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
+        pagination
+        autoPageSize
         rows={newDataframe}
         columns={columns}
         components={{
