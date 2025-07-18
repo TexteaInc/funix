2ec99589a19cbbae08aa59f0d486560f84afd260
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2025-07-18 08:11:00 +0800

    fix(theme): make app bar white

--- frontend/src/shared/theme.ts
+++ frontend/src/shared/theme.ts
@@ -2,117 +2,14 @@ import { Theme } from "@mui/material";
 import _ from "lodash";
 
 const defaultLightTheme = {
-  palette: {
-    type: "light",
-    primary: {
-      main: "#fefefe",
-      light: "#ffffff",
-      dark: "#dfdfdf",
-    },
-    secondary: {
-      main: "#e040fb",
-    },
-    background: {
-      paper: "#fefefe",
-    },
-  },
   components: {
-    MuiSvgIcon: {
+    MuiAppBar: {
       styleOverrides: {
         root: {
-          color: "inherit !important",
-        },
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        textPrimary: {
-          color: "#1976d2",
-        },
-      },
-    },
-    MuiCheckbox: {
-      styleOverrides: {
-        root: {
-          color: "rgba(0, 0, 0, 0.6)",
-          "&.Mui-checked": {
-            color: "rgb(0, 0, 0)",
-          },
-        },
-      },
-    },
-    MuiSlider: {
-      styleOverrides: {
-        root: {
-          color: "rgb(0, 0, 0) !important",
-        },
-      },
-    },
-    MuiInput: {
-      styleOverrides: {
-        root: {
-          "&::after": {
-            borderBottomColor: "rgb(0, 0, 0, 0.87) !important",
-          },
-        },
-        underline: {
-          "&:before": {
-            borderColor: "rgba(0, 0, 0, 0.42) !important",
-          },
-          "&:hover::before": {
-            borderColor: "rgba(0, 0, 0, 0.87) !important",
-          },
-        },
-      },
-    },
-    MuiFormLabel: {
-      styleOverrides: {
-        root: {
-          "&.Mui-focused": {
-            color: "rgb(0, 0, 0, 0.87) !important",
-          },
-        },
-      },
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "& fieldset": {
-            borderColor: "rgba(0, 0, 0, 0.42) !important",
-          },
-          "&&:hover fieldset": {
-            borderColor: "rgba(0, 0, 0, 0.87) !important",
-            borderWidth: "2px !important",
-          },
-        },
-      },
-    },
-    MuiRadio: {
-      styleOverrides: {
-        root: {
-          "&.Mui-checked": {
-            color: "rgb(0, 0, 0) !important",
-          },
-        },
-      },
-    },
-    MuiSwitch: {
-      styleOverrides: {
-        track: {
-          backgroundColor: "rgb(0, 0, 0) !important",
-          "&.Mui-checked": {
-            backgroundColor: "rgb(0, 0, 0) !important",
-          },
-        },
-        textPrimary: {
-          color: "rgb(0, 0, 0) !important",
-        },
-      },
-    },
-    MuiLink: {
-      styleOverrides: {
-        root: {
-          color: "#1976d2",
+          backgroundColor: "#ffffff",
+          color: "inherit",
+          boxShadow:
+            "0px 2px 4px -1px rgba(25,118,210,0.2),0px 4px 5px 0px rgba(25,118,210,0.14),0px 1px 10px 0px rgba(25,118,210,0.12)",
         },
       },
     },
